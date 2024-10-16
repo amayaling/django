3b6438cc4b6f8d36e4d7634425947d8b9e42a01f
Amaya Ling <123601690+amayaling@users.noreply.github.com>
2024-10-15 21:57:36 -0700

    added more documentation

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,12 +6,15 @@ from .tasks import send_query
 
 def query(request):
     queries = request.GET.dict()
-    print(queries)
+    print(queries) # goes to terminal; personal touch to see what code was doing 
     send_query.delay(queries)
-    # this prints the key value pairs and not just the key 
+    # this returns the key value pairs and not just the key 
+    # return HttpResponse(queries) = returns the param and not the value 
     return JsonResponse(queries)
 
-
+'''
+IGNORE EVERYTHING BEOLOW 
+'''
 #     send_req.delay
 #     "Hello from Celery!",
 #     "This is an email sent using Celery and Django.",
